4d5f1d39dc64f52730f6a8c9aa327a5e45784d9e
Andrey Saveliev <freaddy@yandex.ru>
2022-10-15 13:16:22 +0700

    new struct project

--- serv_deamon.cpp
+++ serv_deamon.cpp
@@ -1,27 +1,13 @@
 #include "cored.h"
 #include <iostream>
 #include <string>
-
+/*
+serv_deamon или service
+  основная утилита по управлению демоном, парсит входную строку,и запускает
+*/
 int main(int argc,char*argv[])
 {
 
-core::m_clock random(12,23,15);
-core::m_clock cur(time(0));
-core ::m_clock ziro;
 
-//std::cout<<random<<"\n";
-//std::cout<<cur<<"\n";
-//std::cout<<ziro<<"\n";
-
-std::string s;
-int n=0;
-while (n<3) {
-
-
-std::cin>>s>>ziro;
-
-std::cout<<ziro<<" "<<s<<"\n";
-s.clear();
-++n;}
 return 0;
 }
